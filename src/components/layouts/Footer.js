9eaf96a7ef50b67b505f0d0edfe8d3c2eb37069f
luiizsilverio <luiiz.silverio@gmail.com>
2023-07-17 19:46:31 -0300

    Doc: Readme

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -6,15 +6,15 @@ const Footer = () => {
                 <div className="col-md-10 col-lg-8 col-xl-7">
                     <ul className="list-inline text-center">
                         <li className="list-inline-item">
-                            <a href="#!">
+                            <a href="https://www.linkedin.com/in/luiz-silv%C3%A9rio-de-oliveira-6b6067210/">
                                 <span className="fa-stack fa-lg">
                                     <i className="fas fa-circle fa-stack-2x"></i>
-                                    <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                                    <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
                                 </span>
                             </a>
                         </li>
                         <li className="list-inline-item">
-                            <a href="#!">
+                            <a href="https://pt-br.facebook.com/">
                                 <span className="fa-stack fa-lg">
                                     <i className="fas fa-circle fa-stack-2x"></i>
                                     <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -22,7 +22,7 @@ const Footer = () => {
                             </a>
                         </li>
                         <li className="list-inline-item">
-                            <a href="#!">
+                            <a href="https://github.com/luiizsilverio" target="_blank" rel="noreferrer">
                                 <span className="fa-stack fa-lg">
                                     <i className="fas fa-circle fa-stack-2x"></i>
                                     <i className="fab fa-github fa-stack-1x fa-inverse"></i>
@@ -30,7 +30,9 @@ const Footer = () => {
                             </a>
                         </li>
                     </ul>
-                    <div className="small text-center text-muted fst-italic">Copyright &copy; Your Website 2023</div>
+                    <div className="small text-center text-muted fst-italic">
+                      Copyright &copy; LuizDev 2023
+                    </div>
                 </div>
             </div>
         </div>
